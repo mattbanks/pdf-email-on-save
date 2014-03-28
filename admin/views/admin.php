d450e774dd632db655a1062aba77d11b9b2efb9f
Matt Banks <mjbanks@gmail.com>
2014-03-28 11:14:55 -0400

    initial commit

---
+++ admin/views/admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   Plugin_Name
+ * @author    Matt Banks <mjbanks@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://mattbanks.me
+ * @copyright 2014 Your Name or Company Name
+ */
+?>
+
+<div class="wrap">
+
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+	<!-- @TODO: Provide markup for your options page here. -->
+
+</div>
