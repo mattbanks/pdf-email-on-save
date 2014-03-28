b6d3a444ff928b833602c595f00e8180ddd93186
Matt Banks <mjbanks@gmail.com>
2014-03-28 11:37:51 -0400

    begin setting up public functionality

--- public/class-pdf-email-on-save.php
+++ public/class-pdf-email-on-save.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name.
+ * PDF Email on Save
  *
- * @package   Plugin_Name
+ * @package   PDF_Email
  * @author    Matt Banks <mjbanks@gmail.com>
  * @license   GPL-2.0+
  * @link      http://mattbanks.me
@@ -10,11 +10,7 @@
  */
 
 /**
- * Plugin class. This class should ideally be used to work with the
- * public-facing side of the WordPress site.
- *
- * If you're interested in introducing administrative or dashboard
- * functionality, then refer to `class-pdf-email-on-save-admin.php`
+ * PDF Email on Save class
  *
  * @package PDF_Email
  * @author  Matt Banks <mjbanks@gmail.com>
@@ -31,8 +27,6 @@ class PDF_Email {
 	const VERSION = '1.0.0';
 
 	/**
-	 * @TODO - Rename "pdf-email-on-save" to the name your your plugin
-	 *
 	 * Unique identifier for your plugin.
 	 *
 	 *
@@ -69,15 +63,10 @@ class PDF_Email {
 		// Activate plugin when new blog is added
 		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
 
-		// Load public-facing style sheet and JavaScript.
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-
-		/* Define custom functionality.
-		 * Refer To http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+		/*
+		 * Trigger PDF creation and emailing when appropriate posts are saved
 		 */
-		add_action( '@TODO', array( $this, 'action_method_name' ) );
-		add_filter( '@TODO', array( $this, 'filter_method_name' ) );
+		add_action( 'save_post', array( $this, 'create_pdf' ) );
 
 	}
 
@@ -260,47 +249,49 @@ class PDF_Email {
 	}
 
 	/**
-	 * Register and enqueue public-facing style sheet.
+	 * Get post types saved in 'mb_pdf_email_post_types' option
 	 *
 	 * @since    1.0.0
+	 *
+	 * @return   array    Array of post types
 	 */
-	public function enqueue_styles() {
-		wp_enqueue_style( $this->plugin_slug . '-plugin-styles', plugins_url( 'assets/css/public.css', __FILE__ ), array(), self::VERSION );
+	private function get_post_types() {
+		// @TODO: Get post types saved in 'mb_pdf_email_post_types' option
 	}
 
 	/**
-	 * Register and enqueues public-facing JavaScript files.
+	 * Get the email address to use saved in 'mb_pdf_email_address' option
 	 *
 	 * @since    1.0.0
+	 *
+	 * @return   string    Email addresses for mailing PDF
 	 */
-	public function enqueue_scripts() {
-		wp_enqueue_script( $this->plugin_slug . '-plugin-script', plugins_url( 'assets/js/public.js', __FILE__ ), array( 'jquery' ), self::VERSION );
+	private function get_email_address() {
+		// @TODO: Get the email address to use saved in 'mb_pdf_email_address' option
 	}
 
 	/**
-	 * NOTE:  Actions are points in the execution of a page or process
-	 *        lifecycle that WordPress fires.
-	 *
-	 *        Actions:    http://codex.wordpress.org/Plugin_API#Actions
-	 *        Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
+	 * Email PDF to user
 	 *
 	 * @since    1.0.0
 	 */
-	public function action_method_name() {
-		// @TODO: Define your action hook callback here
+	private function send_email() {
+		// @TODO: Send email
 	}
 
 	/**
-	 * NOTE:  Filters are points of execution in which WordPress modifies data
-	 *        before saving it or sending it to the browser.
-	 *
-	 *        Filters: http://codex.wordpress.org/Plugin_API#Filters
-	 *        Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
+	 * Create PDF from appropriate post type
 	 *
 	 * @since    1.0.0
 	 */
-	public function filter_method_name() {
-		// @TODO: Define your filter hook callback here
+	public function create_pdf( $post_id ) {
+		// @TODO: Get post types saved in 'mb_pdf_email_post_types' option
+
+		// @TODO: Create the PDF
+
+		// @TODO: Get the email address to use saved in 'mb_pdf_email_address' option
+
+		// @TODO: Send email
 	}
 
 }
